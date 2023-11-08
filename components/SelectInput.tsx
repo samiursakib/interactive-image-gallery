99490f652b4c954343e45073baedd18ebc700f58
samiursakib <rafisamiur@gmail.com>
2023-11-08 20:14:31 +0600

    hover on checkbox fixed

--- components/SelectInput.tsx
+++ components/SelectInput.tsx
@@ -4,6 +4,7 @@ import { ItemType, useItemsContext } from '@/providers/ItemsContextProvider';
 
 const CheckBox = ({ item }: { item: ItemType }) => {
   const { state, setState } = useItemsContext();
+  const selectedItems = state.filter(i => i.isSelected);
   const isSelected = state.find(i => i.id === item.id)!.isSelected;
   const handleCheck = () => {
     setState(state => state.map(i => i.id === item.id ? { ...i, isSelected: !isSelected } : i));
@@ -11,7 +12,7 @@ const CheckBox = ({ item }: { item: ItemType }) => {
 
   return (
     <input
-      className='absolute top-1 left-1 form-checkbox rounded text-[#36c0e6] focus:ring-transparent z-10 invisible group-hover:visible'
+      className={`absolute top-1 left-1 form-checkbox rounded text-[#36c0e6] focus:ring-transparent z-10 group-hover:visible ${selectedItems.length > 0 && isSelected? 'visible' : 'invisible'}`}
       type='checkbox'
       checked={isSelected}
       onChange={handleCheck}
